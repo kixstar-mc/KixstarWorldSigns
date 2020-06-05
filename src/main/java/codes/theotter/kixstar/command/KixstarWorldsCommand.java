14c9b3fc6d9bfe9c5bc42494357b69be8903bc90
Carlo Field <carlo@swiss.dev>
2020-06-05 12:48:02 +0200

    refactor worlds command, remove duplicate code

--- src/main/java/codes/theotter/kixstar/command/KixstarWorldsCommand.java
+++ src/main/java/codes/theotter/kixstar/command/KixstarWorldsCommand.java
@@ -24,40 +24,29 @@ public class KixstarWorldsCommand extends KixstarCommand implements CommandExecu
             return false;
         }
 
-        if (sub_command.equalsIgnoreCase("add")) {
-            if (args.length < 3) {
-                return false;
-            }
-
-            Player player = Bukkit.getPlayer(args[1]);
-            String sign_name = args[2].trim();
-            String config_path = "restricted_signs." + sign_name;
-            List<String> permitted_players = (ArrayList<String>) KixstarWorldSigns.getInstance().getConfig().getList(config_path, new ArrayList<String>());
-            permitted_players.add(player.getUniqueId().toString());
-            KixstarWorldSigns.getInstance().getConfig().set(config_path, permitted_players);
-            KixstarWorldSigns.getInstance().saveConfig();
-            this.sendMessage(sender, "&aAdded " + player.getName() + " to sign: " + sign_name);
-
-            return true;
+        if (args.length < 3) {
+            return false;
         }
 
-        if (sub_command.equalsIgnoreCase("remove")) {
-            if (args.length < 3) {
-                return false;
-            }
+        Player player = Bukkit.getPlayer(args[1]);
+        String sign_name = args[2].trim();
+        String config_path = "restricted_signs." + sign_name;
+        List<String> permitted_players = (ArrayList<String>) KixstarWorldSigns.getInstance().getConfig().getList(config_path, new ArrayList<String>());
 
-            Player player = Bukkit.getPlayer(args[1]);
-            String sign_name = args[2].trim();
-            String config_path = "restricted_signs." + sign_name;
-            List<String> permitted_players = (ArrayList<String>) KixstarWorldSigns.getInstance().getConfig().getList(config_path, new ArrayList<String>());
+        if (sub_command.equalsIgnoreCase("add")) {
+            permitted_players.add(player.getUniqueId().toString());
+            this.sendMessage(sender, "&aAdded " + player.getName() + " to sign: " + sign_name);
+        } else if (sub_command.equalsIgnoreCase("remove")) {
             permitted_players.remove(player.getUniqueId().toString());
-            KixstarWorldSigns.getInstance().getConfig().set(config_path, permitted_players);
-            KixstarWorldSigns.getInstance().saveConfig();
             this.sendMessage(sender, "&aRemoved " + player.getName() + " to sign: " + sign_name);
 
-            return true;
+        } else {
+            return false;
         }
 
-        return false;
+        KixstarWorldSigns.getInstance().getConfig().set(config_path, permitted_players);
+        KixstarWorldSigns.getInstance().saveConfig();
+        return true;
+
     }
 }
